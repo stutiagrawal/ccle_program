78b9f03f426798bc2d7f1f7371ef2f6b33023b13
Stuti Agrawal <stuti.agrawal@gmail.com>
2015-02-27 23:21:45 +0000

    some changes

--- ccle.py
+++ ccle.py
@@ -39,34 +39,43 @@ def select_columns(col_names, df):
             df = df.drop(column, axis=1)
     return df.sort(axis=1)
 
+def convert_to_df_and_sort(dictionary, col_name):
+    tmp = pandas.Series(dictionary)
+    tmp = pandas.DataFrame(tmp, columns=[col_name])
+    tmp = tmp.sort(columns=col_name)
+    return tmp
+
 def get_correlation(expr, drug, drug_name):
     rows_expr, cols_expr = expr.shape
     rows_drug, cols_drug = drug.shape
     corr = dict()
+    p = dict()
     print "Num of celllines in expr: %d" %cols_expr
     print "Num of cell lines in drug: %d" %cols_drug
     if(cols_expr == cols_drug):
         for gene in expr.index:
-            print gene, repr(gene)
             (pearson_coeff, p_value) = scipy.stats.pearsonr(expr.loc[gene], drug.loc[drug_name])
             corr[gene] = pearson_coeff
+            p[gene] = p_value
     else:
         raise Exception("number of cell lines do not match")
-    print corr
-    corr = pandas.Series(corr)
-    corr = pandas.DataFrame(corr, columns=["pearson"])
-    corr = corr.sort(columns="pearson")
-    print corr
-    return corr
+    corr = convert_to_df_and_sort(corr, "pearson")
+    p = convert_to_df_and_sort(p, "p_value")
+    combine = pandas.concat([corr, p], axis =1)
+    combine = combine.sort(columns="pearson")
+    return combine
 
-def get_updated_expr(expr, cv_sel):
+def get_updated_expr(expr, genes_sel):
     """ build a new matrix with selected genes"""
 
-    rows, cols = expr.shape
-    for i in xrange(rows):
-        if i not in cv_sel.keys():
-            expr = expr.drop(i, axis=0)
-    print expr
+    to_be_removed = set()
+
+    for gene_name in expr.index:
+        if gene_name not in genes_sel.index:
+            to_be_removed.add(gene_name)
+
+    for gene_name in to_be_removed:
+        expr = expr.drop(gene_name, axis=0)
     return expr
 
 #create a heatmap of that matrix
@@ -108,29 +117,38 @@ if __name__ == "__main__":
     to_be_removed = set()
     for i in xrange(len(drug.loc[args.drug])):
         if drug.loc[args.drug][i] == -1:
-            print drug.keys()[i]
             to_be_removed.add(drug.keys()[i])
 
     for cell_line in to_be_removed:
         drug = drug.drop(cell_line, axis=1)
 
     #cv_sel = get_coef_var(expr, args.n_features)
-    #expr = get_updated_expr(expr, cv_sel)
     print "selecting cell lines"
     drug_cell_lines = get_col_names(drug)
     expr = select_columns(drug_cell_lines, expr)
     expr_cell_lines = get_col_names(expr)
     drug = select_columns(expr_cell_lines, drug)
 
-    #for value in drug.keys():
-    #    print value
-    #print expr.ix[0:10, :]
     print "getting correlation"
-    corr_sorted = get_correlation(expr, drug, args.drug)
-    print corr_sorted
+    correlation = get_correlation(expr, drug, args.drug)
+    print len(correlation)
+    min_corr = correlation.ix[0:args.n_features/2, :]
+    max_corr = correlation.ix[len(correlation) - args.n_features/2:, :]
+    sel = pandas.concat([min_corr, max_corr], axis=0)
+    expr = get_updated_expr(expr, sel)
+    print sel
+    fp = open("cell_line.txt", "r")
+    cell_line_names = dict()
+    for line in fp:
+        line = line.rstrip().split()
+        if len(line) == 2:
+            cell_line_names[line[1]] = line[0]
+    expr = expr.rename(columns=cell_line_names)
+    #for gene in sel.index:
+    #    print "%s\t%s\t%s\n" %(gene, p.ix[gene]["p_value"], corr_sorted[gene]["pearson"])
     #labels = expr["Description"]
     #expr = pandas.DataFrame(expr, index=labels)
-    #expr.to_csv("gnf_out_1.txt", sep="\t")
+    expr.to_csv("heatmap.txt", sep="\t")
     #create_heatmap(expr)
 
 
